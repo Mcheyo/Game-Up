7003dbeae6451b07d5a009ad952350b74965c427
Anna Dybas <a.m.dybas@gmail.com>
2020-02-29 14:52:59 -0500

    can seed 400 games

--- Game-Up-Backend/db/seeds.rb
+++ Game-Up-Backend/db/seeds.rb
@@ -10,37 +10,34 @@ require 'byebug'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Game.destroy_all
+#Game.destroy_all
 # User.destroy_all
 # User_Game.destroy_all
 
 
-def getGames
-    all_games = RestClient.get('https://api.rawg.io/api/games?page=1&page_size=40') 
-    game_hash = JSON.parse(all_games)
-    
-    game_hash["results"]
-
-    # return game_hash
+def get_games
+    game_hash = []
+    (1...10).each do |i|
+        all_games = RestClient.get("https://api.rawg.io/api/games?page=#{i}&page_size=40")
+        game_hash = game_hash + JSON.parse(all_games)["results"]
+    end
+    game_hash
 end 
 
-def getGameDetail
-    getGames.map do |game| 
-    database_id = game["id"]    
-    name = game["name"]
-    released = game["released"]
-    background_image = game["background_image"]
-    rating = game["rating"]
-    metacritic = game["metacritic"]
-    playtime = game["playtime"]
-    clip = game["clip"]["clip"]
+def get_game_detail
     
+    get_games.map do |game| 
+        database_id = game["id"]    
+        name = game["name"]
+        released = game["released"]
+        background_image = game["background_image"]
+        rating = game["rating"]
+        metacritic = game["metacritic"]
+        playtime = game["playtime"]
+        clip = (game["clip"] === nil ? "youtube.com" : game["clip"]["clip"]) 
     Game.find_or_create_by(database_id: database_id, name: name, released: released, background_image: background_image, rating: rating, metacritic: metacritic, playtime:playtime, clip:clip)
-
-
-
     end
 
 end 
 
-getGameDetail
+get_game_detail
