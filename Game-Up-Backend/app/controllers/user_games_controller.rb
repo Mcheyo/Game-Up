a473fdd8073e2397c8b02de211aae64925a483fb
Anna Dybas <a.m.dybas@gmail.com>
2020-03-03 16:32:21 -0500

    adding user likes

---
+++ Game-Up-Backend/app/controllers/user_games_controller.rb
@@ -0,0 +1,18 @@
+class UserGamesController < ApplicationController
+  
+    def index 
+        user_games = UserGame.all 
+        render json: user_games
+    end 
+
+    def show 
+        user_game = UserGame.find_by(id: params[:id])
+        render json: user_game
+    end 
+
+    def create 
+        user_game = UserGame.create(params.require(:user_game).permit!)
+        render json: user_game
+    end 
+
+end
